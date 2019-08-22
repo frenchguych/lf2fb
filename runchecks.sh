39f5a8e2e94bd260b9a83baf020b2c538908916e
Yannick Mauray <yannick.mauray@arcantel.ch>
2019-08-22 16:37:02 +0200

    codecov integration

--- runchecks.sh
+++ runchecks.sh
@@ -4,4 +4,4 @@ set -e
 
 ./vendor/bin/phpcs --colors --standard=PEAR --exclude=PEAR.Commenting.FileComment,PEAR.Commenting.ClassComment src/
 ./vendor/bin/phpstan --ansi analyze src/
-./vendor/bin/phpunit --bootstrap ./vendor/autoload.php --colors --testdox tests/
+./vendor/bin/phpunit --bootstrap ./vendor/autoload.php --colors --testdox --coverage-clover=coverage.xml tests/
